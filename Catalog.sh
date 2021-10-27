1977ae5973efac691da5415a5994cfd7a36d9b60
Daniel Lemos Simões <38388738+danipresto@users.noreply.github.com>
2021-10-27 11:12:24 -0300

    Update Catalog.sh

--- Catalog.sh
+++ Catalog.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-find /d  -iname "*.jpg" | paste -sd ',\n' >> catalogo.csv
+if [ "$2" == "img" ] 
+then 
+    find $1 -iname "*.jpg" -type f -printf '%p,\n'  >> catalogo_img.csv
+fi    
+
+if [ "$2" == "all" ] 
+then 
+    find $1 -type f -printf '%p,\n' >> catalogo_all.csv
+fi    
+
+#para executar coloque "endereço de varredura" + "img "(para buscar apenas imagens ou "all" (para varrer tudo)
