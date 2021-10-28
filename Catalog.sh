d911d95238a62db4ef2f4a49b4823bc9960f6638
Daniel Lemos Simões <38388738+danipresto@users.noreply.github.com>
2021-10-28 08:46:00 -0300

    Update Catalog.sh
    Nome do arquivo.csv parametrizável por linha de comando

--- Catalog.sh
+++ Catalog.sh
@@ -2,12 +2,12 @@
 
 if [ "$2" == "img" ] 
 then 
-    find $1 -iname "*.jpg" -type f -printf '%p,\n'  >> catalogo_img.csv
+    find $1 -iname "*.jpg" -type f -printf '%p,\n'  >> $3_catalogo_img.csv
 fi    
 
 if [ "$2" == "all" ] 
 then 
-    find $1 -type f -printf '%p,\n' >> catalogo_all.csv
+    find $1 -type f -printf '%p,\n' >> $3_catalogo_all.csv
 fi    
 
 #para executar coloque "endereço de varredura" + "img "(para buscar apenas imagens ou "all" (para varrer tudo)
