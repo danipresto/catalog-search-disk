a7066625846a8adbb7f8eacc8c18be3a6a44c150
Isaacif <isaaclimadesousa08@gmail.com>
2021-11-16 15:22:52 -0300

    Update Ordenar.cpp

--- Ordenar.cpp
+++ Ordenar.cpp
@@ -2,18 +2,20 @@
 #include <iostream>
 #include <vector>
 #include "include/dirent.h"
-#include <thread> 
+#include <sstream>
+#include <set>
 
+//Lê um .csv e separa o conteúdo em pastas
 
-
-/* Lê um .csv já e separa ele em novos por contrato em novos arquivos .csv */
- 
 void writetp(const char* name, std::string lin, std::string path)
 {
 	std::string nam = name;
 	if (lin.find(nam) != std::string::npos)
 	{
+		std::string fname = path + nam;
 		std::string pat = path + nam + "\\" + nam + ".csv";
+		std::string file = fname + "\\" + nam + ".csv";
+		mkdir(fname.c_str());
 		std::ofstream mycont(pat.c_str(), std::ios_base::app);
 		mycont << lin << "\n";
 	}
@@ -25,33 +27,38 @@ int main(int arg, char* argv[])
 	//std::cout << arg << "\n";
 	std::string path = argv[1]; //DIRETORIO PARA SALVAR OS ARQUIVOS
 	std::ifstream catalog(argv[2]); //LOCAL DO ARQUIVO .CSV
-	std::vector<std::string> Contracts = {"amc", "detran", "goiania", "maracanau"}; //Falta Adicionar todos os contratos
-	std::fstream logs; std::string line;
+	std::set<std::string> direct;  std::vector<std::string> content;
+	std::fstream logs; std::string line; std::string part;
 
 	
-	for (std::string elem : Contracts) {
-		std::string fname = path + elem;
-		std::string file = fname + "\\" + elem + ".csv";
-		mkdir(fname.c_str());
-		std::fopen(file.c_str(), "w");
-	}	
-
+	
 	while (std::getline(catalog, line))
 		{	
+        	std::stringstream nline(line);
+			int i = 0;
+          	while(std::getline(nline, part, '/'))
+			{	 
+  			 	content.push_back(part);
+				if (i == 4){
+					break;
+				}
+				i++;
+			}
+	
+        
+			
+
 			if(line.find("contratos") != std::string::npos)
 			{	
-				writetp("detran", line, path); 
-				writetp("amc", line, path); 
-				writetp("goiania", line, path); 
-				writetp("goiania-corredor", line, path); 
-				writetp("maracanau", line, path);
-
-			}
+                writetp(content[4].c_str(), line, path);
+            } 
 			else 
 			{
 				std::cout << "Não é Parte de contratos/" << std::endl;
 			}
+
+			content.clear();
 		}
-}
+    }
 
 
