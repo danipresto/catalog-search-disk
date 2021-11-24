60c089c2e0f5e6d02b65c5a80a7127aa5481b1e2
Isaacif <isaaclimadesousa08@gmail.com>
2021-11-24 17:24:21 -0300

    Update Catalog.cpp

--- CatalogC++/Catalog.cpp
+++ CatalogC++/Catalog.cpp
@@ -1,132 +1,45 @@
-/*AINDA EM IMPLEMENTAÇÃO*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h> //Biblioteca de diretórios 
+#include <string>
 #include <iostream>
-#include <chrono>   //Biblioteca de medições de tempo
-#include <fstream>  //Biblioteca de leitura e escrita de arquivos
-
-
-auto start = std::chrono::high_resolution_clock::now(); //Inicia a contagem
-/* Função p/ listar diretórios. 
-Recebe o nome do diretório, um inteiro para escrever a ordem no csv 
-e o objeto arquivo */
-
-static int find_direct(const char* dirname, std::ofstream* myfile) 
-{
-    DIR* dir; 
-    char buffer[PATH_MAX + 2]; //Array com tamanho máximo de subdiretórios a pesquisar
-    char* p = buffer; //Ponteiro na posição inicial da array
-    const char* src; 
-    char* end = &buffer[PATH_MAX];
-    int ok;
-
-
-    src = dirname;
-    while (p < end && *src != '\0') { //Enquanto o ponteiro p não estiver na última posição incrementar o valor de p; 
-        *p++ = *src++;
-    }
-    *p = '\0';
-
-    dir = opendir(dirname); //Abri o diretório e armazena em objeto do tipo DIR
-    if (dir != NULL) {
-        struct dirent* ent;
-
-        while ((ent = readdir(dir)) != NULL) { //Enquanto houver subdiretórios em dir
-            char* q = p;
-            char c;
-
-            if (buffer < q) {
-                c = q[-1];
-            }
-            else {
-                c = ':';
-            }
-
-         
-            if (c != ':' && c != '/' && c != '\\') {
-                *q++ = '/';
-            }
-
-           
-            src = ent->d_name;
-            while (q < end && *src != '\0') {
-                *q++ = *src++;
-            }
-            *q = '\0';
-
-            switch (ent->d_type) {
-            case DT_LNK:
-            case DT_REG:
-                
-               
-                *myfile << buffer << "\n";
-
-                break;
-
-            case DT_DIR:
-               
-                if (strcmp(ent->d_name, ".") != 0
-                    && strcmp(ent->d_name, "..") != 0) {
-                    find_direct(buffer, myfile);
-                }
-                break;
-
-            default:;
-            }
-
-        }
-
-        closedir(dir);
-        ok = 1;
-
-    }
-    else {
-       
-        printf("Cannot open directory %s\n", dirname);
-        ok = 0;
-    }
-
-    return ok;
-}
-
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
 
 int main(int argc, char* argv[])
 {
-    bool CurrentDt = true; //usar comando de linha ou a variável directory como inicio. false usa a variável.
-    int i;
-    int ok;
-    const char* directory = "D:\\";
-    std::ofstream catalog;
-    catalog.open("C:\\Users\\isaacsousa\\Desktop\\Isaac\\catalog.csv"); //diretório para criação do .csv
+    std::string file = argv[2];
+    std::string path = argv[1];
+    std::string line;
+    std::vector<std::string> paths;
+    const char* pch;
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
+        paths = {};
+        char* cstr = new char[entry.path().string().length() + 1];
+        strcpy(cstr, entry.path().string().c_str());
+        pch = strtok(cstr, "/ \\");
+        while (pch != NULL)
+        {
 
-    i = 1;
-    if (CurrentDt) {
-        while (i < argc) {
-            ok = find_direct(argv[i], &catalog);
-         
-            if (!ok) {
-                exit(EXIT_FAILURE);
+            paths.push_back(pch);
+            pch = strtok(NULL, "/ \\");
+        }
+        if (paths.size() >= 4) {   
+            if (!std::filesystem::exists(file + "/" + paths[2])) {
+                std::filesystem::create_directory(file+ "/" + paths[2]);
             }
-            i++;
-        }
 
-        if (argc == 1) {
-            find_direct(".", &catalog);
+            if (!std::filesystem::exists(file + "/" + paths[2] + "/" + paths[3])) {
+                std::filesystem::create_directory(file + "/" + paths[2] + "/" + paths[3]);
+            }
+
+            std::string nwf = file + "/" + paths[2] + "/" + paths[3] + "/" + paths[3] + ".csv";
+            std::ofstream catalog(nwf.c_str(), std::ios_base::app);
+            catalog << entry.path().string() << '\n';
         }
     }
-    else {
-        find_direct(directory, &catalog);
-    }
-
-    catalog.close();
-    auto stop = std::chrono::high_resolution_clock::now(); //Para de contar o tempo
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
-    std::cout << "Tempo gasto : " << duration.count() << "Segundos" << std::endl; 
-    return 0;
 }
 
-
-/* Uso: g++ Local\\Catalog.cpp -o nome.exe
-   nome.exe diretório a buscar */
+/* Compilar usando: g++ Catalog.cpp -std=c++17 -o Catalog.exe
+   Executar usando: .\Catalog.exe LocaldoCatalogo Localdo.csv */
